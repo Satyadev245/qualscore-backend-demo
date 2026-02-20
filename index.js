a0f1e7d143ba551b45f859c91e08a4fe3cbf6439
Satyadev245 <dev.satya.me@gmail.com>
2026-02-20 11:59:31 +0530

    changes

--- index.js
+++ index.js
@@ -96,6 +96,8 @@ app.post("/api/book-demo", async (req, res) => {
    Server Start
 ================================ */
 
-app.listen(PORT, "0.0.0.0", () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
